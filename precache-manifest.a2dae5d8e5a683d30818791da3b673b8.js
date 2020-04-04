657548f01797ce29291a4f330ed390f36154ca6c
i829550 <john.jiang01@sap.com>
2020-04-03 21:14:42 -0400

    initial commit

---
+++ precache-manifest.a2dae5d8e5a683d30818791da3b673b8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "329a0b4faf512cf6b3a9ad8807e38c68",
+    "url": "/index.html"
+  },
+  {
+    "revision": "63496a57b75761fce422",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "afcf742ae63e650f96c7",
+    "url": "/static/js/2.8e60413b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8e60413b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63496a57b75761fce422",
+    "url": "/static/js/main.6a1e0228.chunk.js"
+  },
+  {
+    "revision": "da25c7344ceaf63fbe1a",
+    "url": "/static/js/runtime-main.f9a736b6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
